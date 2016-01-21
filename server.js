589a00d680865037892c3a52a4fdd9fb608c53cb
Dan Siwiec <daniel.siwiec@gmail.com>
2016-01-21 01:18:40 -0800

    Add Procfile for Heroku

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ server.get('/', (req, res, next) => {
   return next();
 });
 
-server.listen(8080, () => {
+server.listen(process.env.PORT || 8080, () => {
   console.log(`${server.name} listening at ${server.url}`);
 });
